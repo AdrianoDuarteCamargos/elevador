65fd6fc488f0e4b6c9f005e9b1105000ebaa6689
AdrianoDuarteCamargos <adrianoduartebh@gmail.com>
2024-06-09 14:56:13 -0300

    parada do elevador

---
+++ elevador.py
@@ -0,0 +1,17 @@
+for i in range(1,21):
+  if (i ==13):
+    continue
+  print(i)
+print('Aqui é o térreo')
+i = 1
+while i <= 20:
+    if i == 13:
+        i= i+1
+        continue
+    print('Este andar é o de número: '+str(i))
+    i = i+1
+for i in range(20,0,-1):
+  if (i ==13):
+    continue
+  print('Este andar é o de número: '+str(i))
+print('Aqui é o térreo')
